9dd82c093174a2b7eea3f239ab1b3d4fde679aff
milozanoo22-cpu <milozanoo22@gmail.com>
2025-10-04 11:58:42 -0500

    Update scritp.js

--- scritp.js
+++ scritp.js
@@ -10,7 +10,7 @@ const API_BASE_URL = window.location.hostname === 'localhost'
 // Configuración de Twitch
 const TWITCH_CONFIG = {
     clientId: 'q2wff2pvt5waw4440bpwyxapppq5vm',
-    redirectUri: 'https://milozanoo22-cpu.github.io/PhanStreamers', // ⬅️ Cambiar esto
+    redirectUri: 'https://milozanoo22-cpu.github.io/PhanStreamers/callback.html', // ⬅️ Cambiar esto
     scopes: ['user:read:email', 'chat:read', 'channel:read:subscriptions']
 };
 
@@ -646,6 +646,7 @@ window.addEventListener('load', function() {
 // Auto-guardar datos cada 30 segundos
 
 setInterval(saveUserData, 30000);
+
 
 
 
